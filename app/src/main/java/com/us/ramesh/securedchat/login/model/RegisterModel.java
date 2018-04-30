a8b6aa820b84eadb9c9d1a887ad4ff44b81856a9
Ramesh <giriramesh120@gmail.com>
2018-04-30 21:37:54 +0545

    Initial push

---
+++ app/src/main/java/com/us/ramesh/securedchat/login/model/RegisterModel.java
@@ -0,0 +1,100 @@
+package com.us.ramesh.securedchat.login.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by ramesh on 3/13/18.
+ */
+
+public class RegisterModel implements Serializable {
+
+    String id, fullname, email,mobno,review,wages, address, profession, experience,userImage,thumbImage;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobno() {
+        return mobno;
+    }
+
+    public void setMobno(String mobno) {
+        this.mobno = mobno;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getWages() {
+        return wages;
+    }
+
+    public void setWages(String wages) {
+        this.wages = wages;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
+
+    public String getThumbImage() {
+        return thumbImage;
+    }
+
+    public void setThumbImage(String thumbImage) {
+        this.thumbImage = thumbImage;
+    }
+}
